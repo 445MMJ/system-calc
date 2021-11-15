f87615287aa57e20aaa65dcc800926528336d2b3
445MMJ <84955388+445MMJ@users.noreply.github.com>
2021-11-15 12:14:32 +0900

    Update List.js

--- List.js
+++ List.js
@@ -1,5 +1,9 @@
 var List = [
-    { id: 132, name: "サモ" ,class:"騎",np_type:"Arts",np_rate:0.71,np_hit:5},
+    { id: 132, name: "サモ" ,class:"騎",np_type:"Arts",np_rate:0.71,np_hit:5,
+        skill:[
+                {name:"セルリアンライドA",type:"card_up",value:30,turn:3},
+                {name:"終わらぬ夏B",type:"np_charge",value:30,turn:3}
+            ]},
     { id: 145, name: "術ギル" ,class:"術",np_type:"Arts",np_rate:0.16,np_hit:10},    
     { id: 169, name: "不夜キャス" ,class:"術",np_type:"Arts",np_rate:0.51,np_hit:5},    
     { id: 201, name: "アナスタシア" ,class:"騎",np_type:"Arts",np_rate:0.51,np_hit:4},    
@@ -16,4 +20,38 @@ var List = [
     { id: 183, name: "パール" ,class:"槍",np_type:"Quick",np_rate:1.08,np_hit:4},    
     { id: 2680, name: "スペイシュQ" ,class:"讐",np_type:"Quick",np_rate:0.69,np_hit:3},    
     { id: 281, name: "ボイジャー" ,class:"降",np_type:"Quick",np_rate:0.56,np_hit:5},
+
+    { id: 9999 ,name:"PLACE HOLDER",class:"盾",np_type:"Quick",np_rate:0.56,np_hit:5,
+        skill:{name:"セルリアンライドA",type:"card_up",value:30,turn:3}}
 ];
+
+const Artoriabase = [
+    {name:"希望のカリスマB",type:"np_charge",value:30},
+    {name:"アヴァロンの妖精A",type:"np_up",value:30},
+    {name:"聖剣作成EX",type:"card_up",value:50}
+    ]
+
+const Artoria = [
+    {id:1, name:"希望のカリスマB",type:"np_charge",value:30},
+    {id:2, name:"アヴァロンの妖精A",type:"np_up",value:30},
+    {id:3, name:"聖剣作成EX",type:"card_up",value:50},
+    {id:4, name:"希望のカリスマB",type:"np_charge",value:30},
+    {id:5, name:"アヴァロンの妖精A",type:"np_up",value:30},
+    {id:6, name:"聖剣作成EX",type:"card_up",value:50}
+    ]
+
+const Scathachbase = [
+    {name:"原初のルーン",type:"card_up",value:50},
+    {name:"大神の叡智 B+",type:"np_charge",value:50}
+    ]
+
+const Scathach = [
+    {id:1, name:"原初のルーン",type:"card_up",value:50},
+    {id:2, name:"大神の叡智 B+",type:"np_charge",value:50},
+    {id:3, name:"原初のルーン",type:"card_up",value:50},
+    {id:4, name:"大神の叡智 B+",type:"np_charge",value:50},
+    ]
+
+
+
+
