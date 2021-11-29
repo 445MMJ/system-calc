5004c8cb466785666286f5934da0f8cd039f969d
445MMJ <84955388+445MMJ@users.noreply.github.com>
2021-11-29 11:56:22 +0900

    Add files via upload

--- List.js
+++ List.js
@@ -1,95 +1,97 @@
 var List = [
-    { id: 132, name: "サモ" ,class:"騎",np_type:"Arts",np_rate:0.71,np_hit:5,
-        skill:[
-                {id: 13201, name:"セルリアンライドA",type:"card_up",value:30},
-                {id: 13202, name:"終わらぬ夏B",type:"np_charge",value:30}
+    { "id": 132, "name": "サモ" ,"class":"騎","np_type":"Arts","np_rate":0.71,"np_hit":5,
+        "skill":[
+                {"id": 13201, "name":"セルリアンライドA","type":"card_up","value":30},
+                {"id": 13202, "name":"終わらぬ夏B","type":"np_charge","value":30}
             ]},
-    { id: 145, name: "術ギル" ,class:"術",np_type:"Arts",np_rate:0.16,np_hit:10,
-        skill:[
-            {id: 14501, name:"魔杖の支配者",type:"card_up",value:30}
+    { "id": 145, "name": "術ギル" ,"class":"術","np_type":"Arts","np_rate":0.16,"np_hit":10,
+        "skill":[
+            {"id": 14501, "name":"魔杖の支配者","type":"card_up","value":30}
             ]},    
-    { id: 169, name: "不夜キャス" ,class:"術",np_type:"Arts",np_rate:0.51,np_hit:5,
-        skill:[
-            {id: 16901, name:"仮置",type:"other",value:0}
+    { "id": 169, "name": "不夜キャス" ,"class":"術","np_type":"Arts","np_rate":0.51,"np_hit":5,
+        "skill":[
+            {"id": 16901, "name":"夜話の語り手 EX","type":"card_up","value":30}
             ]},    
-    { id: 201, name: "アナスタシア" ,class:"騎",np_type:"Arts",np_rate:0.51,np_hit:4,
-        skill:[
-            {id: 20101, name:"仮置",type:"other",value:0}
+    { "id": 201, "name": "アナスタシア" ,"class":"騎","np_type":"Arts","np_rate":0.51,"np_hit":4,
+        "skill":[
+            {"id": 20101, "name":"透視の魔眼 ","type":"card_up","value":50}
             ]},    
-    { id: 208, name: "ジーク" ,class:"術",np_type:"Arts",np_rate:0.78,np_hit:3,
-        skill:[
-            {id: 20801, name:"仮置",type:"other",value:0}
+    { "id": 208, "name": "ジーク" ,"class":"術","np_type":"Arts","np_rate":0.78,"np_hit":3,
+        "skill":[
+            {"id": 20801, "name":"人工英雄（偽） B+ ","type":"np_up","value":30},
+            {"id": 20802, "name":"魔術 C ","type":"card_up","value":36}
             ]},    
-    { id: 216, name: "ジャンヌ(弓)" ,class:"弓",np_type:"Arts",np_rate:0.68,np_hit:4,
-        skill:[
-            {id: 21601, name:"仮置",type:"other",value:0}
+    { "id": 216, "name": "ジャンヌ(弓)" ,"class":"弓","np_type":"Arts","np_rate":0.68,"np_hit":4,
+        "skill":[
+            {"id": 21601, "name":"エンドレス・エンジョイ・サマー！ A ","type":"card_up","value":30}
             ]},    
-    { id: 237, name: "紫式部" ,class:"術",np_type:"Arts",np_rate:0.45,np_hit:6,
-        skill:[
-            {id: 23701, name:"仮置",type:"other",value:0}
+    { "id": 237, "name": "紫式部" ,"class":"術","np_type":"Arts","np_rate":0.45,"np_hit":6,
+        "skill":[
+            {"id": 23701, "name":"仮置","type":"other","value":0}
             ]},    
-    { id: 253, name: "ロリンチ" ,class:"騎",np_type:"Arts",np_rate:0.59,np_hit:3,
-        skill:[
-            {id: 25301, name:"仮置",type:"other",value:0}
+    { "id": 253, "name": "ロリンチ" ,"class":"騎","np_type":"Arts","np_rate":0.59,"np_hit":3,
+        "skill":[
+            {"id": 25301, "name":"仮置","type":"other","value":0}
             ]},
-    { id: 254, name: "イアソン" ,class:"剣",np_type:"Arts",np_rate:0.37,np_hit:5,
-        skill:[
-            {id: 25401, name:"仮置",type:"other",value:0}
+    { "id": 254, "name": "イアソン" ,"class":"剣","np_type":"Arts","np_rate":0.37,"np_hit":5,
+        "skill":[
+            {"id": 25401, "name":"仮置","type":"other","value":0}
             ]},    
-    { id: 268, name: "スペイシュA" ,class:"讐",np_type:"Arts",np_rate:0.69,np_hit:3,
-        skill:[
-            {id: 26801, name:"仮置",type:"other",value:0}
+    { "id": 268, "name": "スペイシュA" ,"class":"讐","np_type":"Arts","np_rate":0.69,"np_hit":3,
+        "skill":[
+            {"id": 26801, "name":"仮置","type":"other","value":0}
             ]},    
-    { id: 315, name: "ハベ" ,class:"騎",np_type:"Arts",np_rate:0.57,np_hit:3,
-        skill:[
-            {id: 31501, name:"仮置",type:"other",value:0}
+    { "id": 315, "name": "ハベ" ,"class":"騎","np_type":"Arts","np_rate":0.57,"np_hit":3,
+        "skill":[
+            {"id": 31501, "name":"早縫の糸紡ぎ B ","type":"card_up","value":30}
             ]},
     
-    { id: 14, name: "アタランテ" ,class:"弓",np_type:"Quick",np_rate:0.5,np_hit:10,
-        skill:[
-            {id: 1401, name:"仮置",type:"other",value:0}
+    { "id": 14, "name": "アタランテ" ,"class":"弓","np_type":"Quick","np_rate":0.5,"np_hit":10,
+        "skill":[
+            {"id": 1402, "name":"アルカディア越え A ","type":"card_up","value":50},
+            {"id": 1402, "name":"カリュドーン狩り A","type":"np_up","value":50}
             ]},
-    { id: 48, name: "バサスロ" ,class:"狂",np_type:"Quick",np_rate:0.5,np_hit:10,
-        skill:[
-            {id: 4801, name:"仮置",type:"other",value:0}
+    { "id": 48, "name": "バサスロ" ,"class":"狂","np_type":"Quick","np_rate":0.5,"np_hit":10,
+        "skill":[
+            {"id": 4801, "name":"魔力逆流 A","type":"np_up","value":100}
             ]},
-    { id: 183, name: "パール" ,class:"槍",np_type:"Quick",np_rate:1.08,np_hit:4,
-        skill:[
-            {id: 18301, name:"仮置",type:"other",value:0}
+    { "id": 183, "name": "パール" ,"class":"槍","np_type":"Quick","np_rate":1.08,"np_hit":4,
+        "skill":[
+            {"id": 18301, "name":"イマジナリ・アラウンド EX ","type":"card_up","value":30}
             ]},    
-    { id: 2680, name: "スペイシュQ" ,class:"讐",np_type:"Quick",np_rate:0.69,np_hit:3,
-        skill:[
-            {id: 26801, name:"仮置",type:"other",value:0}
+    { "id": 2680, "name": "スペイシュQ" ,"class":"讐","np_type":"Quick","np_rate":0.69,"np_hit":3,
+        "skill":[
+            {"id": 26801, "name":"仮置","type":"other","value":0}
             ]},    
-    { id: 281, name: "ボイジャー" ,class:"降",np_type:"Quick",np_rate:0.56,np_hit:5,
-        skill:[
-            {id: 28101, name:"仮置",type:"other",value:0}
+    { "id": 281, "name": "ボイジャー" ,"class":"降","np_type":"Quick","np_rate":0.56,"np_hit":5,
+        "skill":[
+            {"id": 28101, "name":"スイングバイ A ","type":"card_up","value":20}
             ]}
 ];
 
 const Artoriabase = [
-    {name:"希望のカリスマB",type:"np_charge",value:30},
-    {name:"アヴァロンの妖精A",type:"np_up",value:30},
-    {name:"聖剣作成EX",type:"card_up",value:50}
+    {"name":"希望のカリスマB","type":"np_charge","value":30},
+    {"name":"アヴァロンの妖精A","type":"np_up","value":30},
+    {"name":"聖剣作成EX","type":"card_up","value":50}
     ]
 
 const Artoria = [
-    {id:1, name:"希望のカリスマB",type:"np_charge",value:30},
-    {id:2, name:"アヴァロンの妖精A",type:"np_up",value:30},
-    {id:3, name:"聖剣作成EX",type:"card_up",value:50},
-    {id:4, name:"希望のカリスマB",type:"np_charge",value:30},
-    {id:5, name:"アヴァロンの妖精A",type:"np_up",value:30},
-    {id:6, name:"聖剣作成EX",type:"card_up",value:50}
+    {"id":1, "name":"希望のカリスマB","type":"np_charge","value":30},
+    {"id":2, "name":"アヴァロンの妖精A","type":"np_up","value":30},
+    {"id":3, "name":"聖剣作成EX","type":"card_up","value":50},
+    {"id":4, "name":"希望のカリスマB","type":"np_charge","value":30},
+    {"id":5, "name":"アヴァロンの妖精A","type":"np_up","value":30},
+    {"id":6, "name":"聖剣作成EX","type":"card_up","value":50}
     ]
 
 const Scathachbase = [
-    {name:"原初のルーン",type:"card_up",value:50},
-    {name:"大神の叡智 B+",type:"np_charge",value:50}
+    {"name":"原初のルーン","type":"card_up","value":50},
+    {"name":"大神の叡智 B+","type":"np_charge","value":50}
     ]
 
 const Scathach = [
-    {id:1, name:"原初のルーン",type:"card_up",value:50},
-    {id:2, name:"大神の叡智 B+",type:"np_charge",value:50},
-    {id:3, name:"原初のルーン",type:"card_up",value:50},
-    {id:4, name:"大神の叡智 B+",type:"np_charge",value:50},
+    {"id":1, "name":"原初のルーン","type":"card_up","value":50},
+    {"id":2, "name":"大神の叡智 B+","type":"np_charge","value":50},
+    {"id":3, "name":"原初のルーン","type":"card_up","value":50},
+    {"id":4, "name":"大神の叡智 B+","type":"np_charge","value":50},
     ]
